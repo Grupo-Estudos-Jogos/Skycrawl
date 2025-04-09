e525907e358178d5f0ad1f1ddf45af294e9f451c
Rener Pontes <renercpontes@gmail.com>
2025-04-08 21:53:51 -0300

    feat: Implementado a coloração das hitboxes

--- components/boxes/hitbox.gd
+++ components/boxes/hitbox.gd
@@ -8,3 +8,33 @@ extends Area2D
 		set_deferred("monitorable", toggle)
 
 @export var damage: int
+@export var debug_color: Color = Color("00a4818c")
+
+@onready var collisions: Array[CollisionShape2D] = []
+
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		child_entered_tree.connect(_detect_collision)
+		child_exiting_tree.connect(_forget_collision)
+		_set_color()
+
+
+func _set_color() -> void:
+	for collision in collisions:
+		collision.debug_color = debug_color
+
+
+func _detect_collision(node: Node) -> void:
+	if not node is CollisionShape2D:
+		return
+
+	collisions.push_back(node)
+	node.debug_color = debug_color
+
+
+func _forget_collision(node: Node) -> void:
+	if not node is CollisionShape2D:
+		return
+
+	collisions.erase(node)
