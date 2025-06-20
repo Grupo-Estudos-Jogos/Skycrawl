24b84c65d1632ec2e97f62789e8c58be6b25ed8a
Rener Pontes <renercpontes@gmail.com>
2025-06-20 13:59:50 -0300

    feat: Integrado animações em código, como adicionado a corrida do player

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -6,9 +6,11 @@ extends CharacterBody2D
 @export var gravity: float = 50.0
 @export var jump_force: float = 700.0
 @export var max_speed: float = 300
-@export var acceleration: float = 900
+@export var walk_acceleration: float = 900
+@export var run_acceleration: float = 1000
 @export var friction: float = 800
 
+@onready var animation = $AnimatedSprite2D as AnimatedSprite2D
 
 func _ready() -> void:
 	pass
@@ -19,9 +21,17 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	)
 	if horizontal_dir != 0:
-		velocity.x = move_toward(velocity.x, max_speed * horizontal_dir, acceleration * delta)
+		animation.flip_h = true if horizontal_dir < 0 else false
+
+		if Input.is_action_pressed("run"):
+			velocity.x = move_toward(velocity.x, max_speed * horizontal_dir, run_acceleration * delta)
+			animation.play("run")
+		else:
+			velocity.x = move_toward(velocity.x, max_speed * horizontal_dir, walk_acceleration * delta)
+			animation.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
+		animation.play("idle")
 
 	if not is_on_floor():
 		velocity.y += gravity
